e301458cfe8c8e338ba25b8dfd0b058d0b5f3c7e
Werner <wernerpieter.veldsman@unil.ch>
2022-01-04 16:45:32 +0100

    Sample data unit test

--- tests/testthat/test-load_methods.R
+++ tests/testthat/test-load_methods.R
@@ -1,4 +1,9 @@
-test_that("moultdbtools::load_sample() is functional and the resulting tibble has the correct dimensions",
+test_that("Sample data is in the correct format",
           {
+            #Check dimensions
             expect_equal(dim(load_sample()), c(2000,20))
+            #Check number of character type columns
+            expect_equal(table(sapply(load_sample(),class))["character"][[1]], 15)
+            #Check number of numeric type columns
+            expect_equal(table(sapply(load_sample(),class))["numeric"][[1]], 5)
           })
